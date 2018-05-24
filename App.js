eaad2fecb67208a4309b62bece36e456a6890123
Abraham Chang <soyabrahamc@gmail.com>
2018-05-24 09:27:11 -0400

    YellowBox

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, YellowBox } from 'react-native';
+import _ from 'lodash';
 import { Router } from "./Router";
+
+YellowBox.ignoreWarnings(['Setting a timer']);
+const _console = _.clone(console);
+
+console.warn = message => {
+  if (message.indexOf('Setting a timer') <= -1) {
+    _console.warn(message);
+  }
+};
 
 export default class App extends React.Component {
   render() {
     return (
-        <Router />
+      <Router />
     );
   }
 }
