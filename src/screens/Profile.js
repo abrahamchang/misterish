ca15d709a4dd3ad51637e69498f44b0487dea712
Abraham Chang <soyabrahamc@gmail.com>
2018-06-20 17:22:42 -0400

    Profile Casi Listo. (Estatico 100%)

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,17 +1,60 @@
 import React, { Component } from 'react';
-import { View, Image, Text } from 'react-native';
+import { View, Image, Text, TouchableOpacity } from 'react-native';
 
 import Developing from './Developing';
 
 import { createBottomTabNavigator } from 'react-navigation';
 
 class Profile extends Component {
+
+    state = {
+        tabIndex: 1
+    };
+
+    renderTab(){
+
+        const { tabSectionContainer } = styles;
+        if(this.state.tabIndex === 0 ){
+            return(
+                <View style={tabSectionContainer}>
+                   <Text>A</Text> 
+                </View>
+            );
+        }
+        else if(this.state.tabIndex === 1 /*|| undefined || null*/){
+            return(
+                <View style={tabSectionContainer}>
+                    <Text>B</Text>
+                </View>
+            );
+        }
+        else{
+            return(
+                <View style={tabSectionContainer}>
+                    <Text>C</Text>
+                </View>
+            );
+        }
+    }
+
+    changeTab(toTab){
+        this.setState({ tabIndex: toTab });
+    }
+
+    amIActive(currentTab){
+        return this.state.tabIndex === currentTab;
+    }
+
     render() {
         const {
             imageContainer,
             detailsContainer,
             imageStyle,
-            headerStyle
+            headerStyle,
+            buttonContainerIn,
+            buttonContainerAc,
+            tabContainer,
+            tabSectionContainer
         } = styles;
 
         return (
@@ -24,14 +67,14 @@ class Profile extends Component {
                         />
                     </View>
                     <View style={{ height: '60%' }}>
-                        <Text 
-                        style={{
-                            fontSize: 16,
-                            color: '#553285',
-                            fontWeight: 'bold',
-                            textAlign: 'center'
-                        }}>
-                        Abraham Chang | Lvl. 1
+                        <Text
+                            style={{
+                                fontSize: 16,
+                                color: '#553285',
+                                fontWeight: 'bold',
+                                textAlign: 'center'
+                            }}>
+                            Abraham Chang | Lvl. 1
                         </Text>
 
                         <View style={detailsContainer}>
@@ -39,7 +82,22 @@ class Profile extends Component {
                         </View>
                     </View>
                 </View>
+                <View style={tabContainer}>
+                    <TouchableOpacity style={this.amIActive(0) ? buttonContainerIn : buttonContainerAc  } onPress={() => this.changeTab(0)}>
+                        <Text style={{ color: 'white' }}>Creations</Text>
+                    </TouchableOpacity>
 
+                    <TouchableOpacity style={this.amIActive(1) ? buttonContainerIn : buttonContainerAc  } onPress={() => this.changeTab(1)}>
+                    <Text style={{ color: 'white' }}>Progress</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={this.amIActive(2) ? buttonContainerIn : buttonContainerAc  } onPress={() => this.changeTab(2)}>
+                        <Text style={{ color: 'white' }}>Friends</Text>
+                    </TouchableOpacity>
+                </View>
+    
+                    {this.renderTab()}
+                
             </View>
         );
     }
@@ -58,7 +116,7 @@ const styles = {
         aspectRatio: 1 / 1,
         height: '40%',
     },
-    detailsContainer: {        
+    detailsContainer: {
         flex: 1,
         borderColor: '#553285',
         flexDirection: 'column',
@@ -74,7 +132,37 @@ const styles = {
         flex: 1,
         borderRadius: 50
     },
-
+    buttonContainerIn: {
+        backgroundColor: '#36175E',
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 5,
+        margin: 1
+    },
+    buttonContainerAc: {
+        backgroundColor: '#7B52AB',
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 5,
+        margin: 1
+    },
+    tabContainer: {
+        height: '7%',
+        shadowColor: '#000000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        elevation: 2,
+        flexDirection: 'row',
+    },
+    tabSectionContainer: {
+        backgroundColor: 'yellow',
+        height: '42%',
+        width: '100%',
+        marginBottom: 1,
+        marginTop: 1
+    }
 };
 
 export default Profile;
